977d6250b045e091d31ece628c0ad95d427f5479
Viviana Orjuela <bivianaorjuela@gmail.com>
2023-02-10 19:42:46 -0500

    cambios para cargar en vercel

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
+const corsOptions ={
+    origin:'https://todolist-backend-zeta.vercel.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
 const PORT = process.env.PORT || 5500;
 
@@ -12,7 +17,7 @@ const TodoItemRoute = require('./routes/todoItems')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 connectDB();
 
 app.use('/',TodoItemRoute)
