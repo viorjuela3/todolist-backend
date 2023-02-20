2d2fb3ad1c5d52f5f4259836600663d2e199f773
Viviana Orjuela <bivianaorjuela@gmail.com>
2023-02-20 15:53:20 -0500

    cambiando origin de index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const corsOptions ={
-    origin:'https://todolist-backend-9awj7llqu-viorjuela3.vercel.app', 
+    origin:'http://localhost:3000/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
