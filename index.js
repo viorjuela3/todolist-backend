050c38548ff7683f6137acd0a3b1684d22763b52
Viviana Orjuela <bivianaorjuela@gmail.com>
2023-02-20 16:11:59 -0500

    comentando cors

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'http://localhost:3000/', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
 
 const PORT = process.env.PORT || 5500;
 
@@ -17,7 +17,8 @@ const TodoItemRoute = require('./routes/todoItems')
 
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 connectDB();
 
 app.use('/',TodoItemRoute)
