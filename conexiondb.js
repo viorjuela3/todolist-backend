b3148f5ebaddd58b30f2b5eea50f88d0071ed401
Viviana Orjuela <bivianaorjuela@gmail.com>
2023-02-21 10:11:31 -0500

    cambios en server en la ruta y se eliminaron otras lineas

--- conexiondb.js
+++ conexiondb.js
@@ -10,18 +10,18 @@ const connectDB = () => {
     });
   
   
-    // Las siguientes funciones permiten determinar el estado de la base de datos: conectada, desconectada o error en la conexión
-    mongoose.connection.on("connected", () => {
-      console.log("Base de datos conectada");
-    });
+    // // Las siguientes funciones permiten determinar el estado de la base de datos: conectada, desconectada o error en la conexión
+    // mongoose.connection.on("connected", () => {
+    //   console.log("Base de datos conectada");
+    // });
   
-    mongoose.connection.on("disconnected", () => {
-      console.log("Base de datos desconectada");
-    });
+    // mongoose.connection.on("disconnected", () => {
+    //   console.log("Base de datos desconectada");
+    // });
   
-    mongoose.connection.on("error", () => {
-      console.log("Error al conectar la base de datos", error.message);
-    });
+    // mongoose.connection.on("error", () => {
+    //   console.log("Error al conectar la base de datos", error.message);
+    // });
   };
   
   module.exports = connectDB;
