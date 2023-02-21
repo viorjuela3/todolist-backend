f478f52991e87d4732f1844ff21ccf3c8df32a35
Viviana Orjuela <bivianaorjuela@gmail.com>
2023-02-21 10:26:29 -0500

    se quito el / de la ruta origen

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const corsOptions ={
-    origin:'https://todolist-frontend-sand.vercel.app/', 
+    origin:'https://todolist-frontend-sand.vercel.app', 
 //     credentials:true,            //access-control-allow-credentials:true
 //     optionSuccessStatus:200
 }
